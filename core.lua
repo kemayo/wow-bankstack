3057d2c620cfa001956b12d418a12e38cca9b4ed
David Lynch <kemayo@gmail.com>
2007-10-28 19:50:57 +0000

    -Generalize core.Stack so it can stack from an arbitrary group of bags to another arbitrary group of bags.  Allowing... -Add bag/bank compression -- merge stacks to fill. ("/compress", "/compress bank") -Add slashcommand to reverse the bankstack, filling bag stacks from the bank.  ("/bankstack reverse") -"/bankstack help" summarizes this.

--- core.lua
+++ core.lua
@@ -20,9 +20,9 @@ for i = 0, NUM_BAG_SLOTS do
 	table.insert(player_bags, i)
 end
 
-local bankitems = {--[[link = true--]]}
-local banklinks = {--[[bag_int = link--]]}
-local banksinks = {--[[bag_int = room_in_slot--]]}
+local target_items = {--[[link = available_slots--]]}
+local target_links = {--[[bag_int = link--]]}
+local target_sinks = {--[[bag_int = room_in_slot--]]}
 local moves = {--[[bag_int(source):bag_int(target),. ..--]]}
 
 local function bag_int(bag, slot) return (bag*1000) + slot end
@@ -37,18 +37,16 @@ local function clear(t)
 	return t
 end
 
-local bank_open = false
 function core:BANKFRAME_OPENED(...)
-	bank_open = true
+	core.bank_open = true
 end
 function core:BANKFRAME_CLOSED(...)
-	bank_open = false
+	core.bank_open = false
 end
 local t = 0
 frame:SetScript("OnUpdate", function()
-	if not bank_open then
-		clear(moves)
-		frame:Hide()
+	if core.bankrequired and not core.bank_open then
+		core.StopStacking("BankStack: The bank is required.")
 	end
 	t = t + arg1
 	if t > 0.3 then
@@ -58,56 +56,116 @@ frame:SetScript("OnUpdate", function()
 end)
 frame:Hide() -- stops OnUpdate from running
 
-function core.Stack()
-	-- This function just creates a list of slots to move to other slots, and passes it off to a worker function to do the moving.
-	if not bank_open then
+local current_link
+local current_target
+function core.BankStack(arg)
+	if arg=="help" then
+		return core.PrintHelp()
+	end
+	if not core.bank_open then
 		DEFAULT_CHAT_FRAME:AddMessage("BankStack: You must be at the bank.", 1, 0, 0)
 		return
 	end
-	for _,bag in pairs(bank_bags) do
+	core.bankrequired = true
+	core.Stack(arg=="reverse" and bank_bags or player_bags, arg=="reverse" and player_bags or bank_bags)
+end
+function core.Compress(arg)
+	if arg=="help" then
+		return core.PrintHelp()
+	end
+	local bags
+	if arg=="bank" then
+		if not core.bank_open then
+			DEFAULT_CHAT_FRAME:AddMessage("BankStack: You must be at the bank to compress your bank bags.", 1, 0, 0)
+			return
+		else
+			bags = bank_bags
+			core.bankrequired = true
+		end
+	else
+		bags = player_bags
+	end
+	core.Stack(bags, bags)
+end
+function core.PrintHelp()
+	DEFAULT_CHAT_FRAME:AddMessage("BankStack: Stacks things.", 1, 1, 1)
+	DEFAULT_CHAT_FRAME:AddMessage("/bankstack -- fills stacks in your bank from your bags", 1, 1, 1)
+	DEFAULT_CHAT_FRAME:AddMessage("/bankstack reverse -- fills stacks in your bags from your bank", 1, 1, 1)
+	DEFAULT_CHAT_FRAME:AddMessage("/compress -- merges stacks in your bags", 1, 1, 1)
+	DEFAULT_CHAT_FRAME:AddMessage("/compress bank -- merges stacks in your bank", 1, 1, 1)
+end
+
+function core.Stack(source_bags, target_bags)
+	-- Fill incomplete stacks in target_bags with items from source_bags
+	-- This function just creates a list of slots to move to other slots, and passes it off to a worker function to do the moving.
+	if current_link or current_target then
+		DEFAULT_CHAT_FRAME:AddMessage("BankStack: A stacker is already running.", 1, 0, 0)
+		return
+	end
+	-- Model the target bags.
+	for _,bag in pairs(target_bags) do
 		local slots = GetContainerNumSlots(bag)
 		for slot=1, slots do
 			local link = link_to_id(GetContainerItemLink(bag, slot))
 			if link then
 				local stack_size = select(8, GetItemInfo(link))
 				local count = select(2, GetContainerItemInfo(bag, slot))
-				-- If this stack is full, or there's nothing in the player bags we can put into it we don't care about it
-				if (GetItemCount(link) > 0) and (count ~= stack_size) then
+				-- If this stack is full we don't care about it
+				-- Sadly, making this more general means we can't filter on GetItemCount(link) here, too.
+				if count ~= stack_size then
 					local id = bag_int(bag, slot)
-					bankitems[link] = GetItemCount(link, true) - GetItemCount(link) --full count of all this item in bank
-					banklinks[id] = link
-					banksinks[id] = stack_size - count
+					target_items[link] = (target_items[link] and target_items[link] or 0) + 1
+					target_links[id] = link
+					target_sinks[id] = stack_size - count
 				end
 			end
 		end
 	end
-	-- Now go through the player's bags...
-	for _,bag in pairs(player_bags) do
+	-- Now go through the source bags...
+	for _,bag in pairs(source_bags) do
 		local slots = GetContainerNumSlots(bag)
 		for slot=1, slots do
 			local link = link_to_id(GetContainerItemLink(bag, slot))
-			if link and bankitems[link] then
+			if link and target_items[link] then
 				--there's an item in this slot *and* we have room for more of it in the bank somewhere
+				local source_slot = bag_int(bag, slot)
 				local count = select(2, GetContainerItemInfo(bag, slot))
-				for bankslot, banklink in pairs(banklinks) do
-					if banklink==link then
+				for target_slot, target_link in pairs(target_links) do
+					if not target_items[link] then break end
+					if target_link == link and target_slot ~= source_slot then -- (can't stack to itself)
 						-- Schedule moving from this slot to the bank slot.
-						table.insert(moves, bag_int(bag, slot) .. ":" .. bankslot)
+						table.insert(moves, source_slot .. ":" .. target_slot)
 						-- Deal with the bank slot:
-						local room = banksinks[bankslot]
+						local room = target_sinks[target_slot]
 						if room > count then
 							-- This bag slot is emptied, and there's still room in the bank slot for more
-							banksinks[bankslot] = room - count
-							bankitems[link] = bankitems[link] - count
+							target_sinks[target_slot] = room - count
+							if target_sinks[source_slot] then
+								target_items[link] = (target_items[link] > 1) and (target_items[link] - 1) or nil
+								target_sinks[source_slot] = nil
+								target_links[source_slot] = nil
+							end
+							break
 						else
 							-- The bank slot will be filled; remove it from future consideration
-							bankitems[link] = (bankitems[link] > room) and (bankitems[link] - room) or nil
-							banksinks[bankslot] = nil
-							banklinks[bankslot] = nil
+							target_items[link] = (target_items[link] > 1) and (target_items[link] - 1) or nil
+							target_sinks[target_slot] = nil
+							target_links[target_slot] = nil
 							if room == count then
-								-- This bag slot is emptied; stop searching the bank
+								-- This bag slot is emptied; stop searching the bank for this item
+								if target_sinks[source_slot] then
+									-- If this source slot is also in the targets, remove it.
+									target_sinks[source_slot] = nil
+									target_links[source_slot] = nil
+									target_items[link] = (target_items[link] > 1) and (target_items[link] - 1) or nil
+								end
 								break
 							else
+								-- Still items in this bag slot; update the count and keep looking in target.
+								if target_sinks[source_slot] then
+									-- If this source slot is also in the targets, update its counts.
+									target_sinks[source_slot] = target_sinks[source_slot] - room
+								end
 								count = count - room
 							end
 						end
@@ -117,9 +175,9 @@ function core.Stack()
 		end
 	end
 	-- clean up the various cache tables
-	clear(bankitems)
-	clear(banklinks)
-	clear(banksinks)
+	clear(target_items)
+	clear(target_links)
+	clear(target_sinks)
 	if #moves > 0 then
 		--unhide the frame to get the moving started in OnUpdates
 		frame:Show()
@@ -128,20 +186,13 @@ function core.Stack()
 		DEFAULT_CHAT_FRAME:AddMessage("BankStack: Nothing to stack.", 1, 1, 1)
 	end
 end
---function core:Compress(bags)
---end
-
-local current_link
-local current_target
 function core.DoMoves()
 	if CursorHasItem() then
 		local _, _, itemlink = GetCursorInfo()
 		local itemlink = link_to_id(itemlink)
 		if (not current_target) or (current_link ~= itemlink) then
 			-- We didn't pick up whatever is on the cursor; things could get really screwed up if we carry on.  Abort!
-			clear(moves)
-			frame:Hide()
-			return
+			return core.StopStacking("BankStack: Confusion. Stopping.")
 		end
 		-- Drop the item into the target slot
 		return PickupContainerItem(int_bag(current_target))
@@ -166,14 +217,29 @@ function core.DoMoves()
 			return PickupContainerItem(source_bag, source_slot)
 		end
 	end
-	frame:Hide()
 	DEFAULT_CHAT_FRAME:AddMessage("BankStack: Complete.", 1, 1, 1)
+	core.StopStacking()
+end
+
+function core.StopStacking(message)
+	core.bankrequired = false
+	current_target = nil
+	current_link = nil
+	clear(moves)
+	frame:Hide()
+	if message then
+		DEFAULT_CHAT_FRAME:AddMessage(message, 1, 0, 0)
+	end
 end
 
 --Slashcommands:
-SlashCmdList["BANKSTACK"] = core.Stack
+SlashCmdList["BANKSTACK"] = core.BankStack
 SLASH_BANKSTACK1 = "/bankstack"
+SlashCmdList["COMPRESSBAGS"] = core.Compress
+SLASH_COMPRESSBAGS1 = "/compress"
+SLASH_COMPRESSBAGS2 = "/compressbags"
 
 --Bindings:
 BINDING_HEADER_BANKSTACK = "BankStack"
 BINDING_NAME_BANKSTACK = "Stack to bank"
+BINDING_NAME_COMPRESS = "Compress bags"
--- fu.lua
+++ fu.lua
@@ -2,11 +2,32 @@ if not (Rock and Rock:HasLibrary("LibFuBarPlugin-3.0")) then return end
 
 local fu = Rock:NewAddon("BankStackFu", "LibFuBarPlugin-3.0")
 fu:SetFuBarOption('tooltipType', "GameTooltip")
---fu:SetFuBarIcon("Interface\\Icons\\INV_Misc_Shovel_01")
-function fu:OnFuBarClick()
-	BankStack.Stack()
+--fu:SetFuBarOption('iconPath', [[Interface\Icons\INV_Misc_Shovel_01]])
+function fu:OnFuBarClick(button)
+	if IsShiftKeyDown() and IsAltKeyDown and IsControlKeyDown() then
+		return BankStack.StopStacking("BankStack: Aborted.")
+	elseif IsShiftKeyDown() then
+		if IsAltKeyDown() then
+			BankStack.BankStack("reverse")
+		else
+			BankStack.BankStack()
+		end
+	else
+		if IsAltKeyDown() then
+			BankStack.Compress("bank")
+		else
+			BankStack.Compress()
+		end
+	end
 end
 function fu:OnUpdateFuBarText()
 	self:SetFuBarText("BankStack")
 end
---/print Rock:GetAddon("BankStackFu"):IsFuBarIconShown()
+function fu:OnUpdateFuBarTooltip()
+	GameTooltip:AddLine("BankStack")
+	GameTooltip:AddDoubleLine("Click", "Compress", 0, 1, 0, 1, 1, 1)
+	GameTooltip:AddDoubleLine("Alt-Click", "Compress bank", 0, 1, 0, 1, 1, 1)
+	GameTooltip:AddDoubleLine("Shift-Click", "Stack to bank", 0, 1, 0, 1, 1, 1)
+	GameTooltip:AddDoubleLine("Alt-Shift-Click", "Stack to bags", 0, 1, 0, 1, 1, 1)
+	GameTooltip:AddDoubleLine("Ctrl-Alt-Shift-Click", "Abort", 0, 1, 0, 1, 0, 0)
+end
