cbb8b0f5a40710ab7ba1f6e971ff59cc178f6e7f
David Lynch <kemayo@gmail.com>
2007-10-30 01:20:37 +0000

    Merge dev branch to trunk.

--- core.lua
+++ core.lua
@@ -21,12 +21,20 @@ for i = 0, NUM_BAG_SLOTS do
 end
 
 local target_items = {--[[link = available_slots--]]}
-local target_links = {--[[bag_int = link--]]}
-local target_sinks = {--[[bag_int = room_in_slot--]]}
-local moves = {--[[bag_int(source):bag_int(target),. ..--]]}
+local target_links = {--[[encode_bagslot = link--]]}
+local target_sinks = {--[[encode_bagslot = room_in_slot--]]}
+local moves = {--[[encode_move(encode_bagslot(),encode_bagslot(target)),. ..--]]}
 
-local function bag_int(bag, slot) return (bag*1000) + slot end
-local function int_bag(int) return math.floor(int/1000), int % 1000 end
+local function encode_bagslot(bag, slot) return (bag*100) + slot end
+local function decode_bagslot(int) return math.floor(int/100), int % 100 end
+local function encode_move(source, target) return (source*10000)+target end
+local function decode_move(move)
+	local s = math.floor(move/10000)
+	local t = move%10000
+	s = (t>9000) and (s+1) or s
+	t = (t>9000) and (t-10000) or t
+	return s, t
+end
 local function link_to_id(link) return link and string.match(link, "item:(%d+)") end -- "item" because we only care about items, duh
 local function clear(t)
 	for k in pairs(t) do
@@ -69,23 +77,31 @@ function core.BankStack(arg)
 	core.bankrequired = true
 	core.Stack(arg=="reverse" and bank_bags or player_bags, arg=="reverse" and player_bags or bank_bags)
 end
-function core.Compress(arg)
-	if arg=="help" then
-		return core.PrintHelp()
+do
+	-- This is a stack filterer.  It's used to stop full stacks being shuffled around
+	-- while compressing bags.
+	local function is_partial(itemid, bag, slot)
+		-- (stacksize - count) > 0
+		return (select(8, GetItemInfo(itemid)) - select(2, GetContainerItemInfo(bag, slot))) > 0
 	end
-	local bags
-	if arg=="bank" then
-		if not core.bank_open then
-			DEFAULT_CHAT_FRAME:AddMessage("BankStack: You must be at the bank to compress your bank bags.", 1, 0, 0)
-			return
-		else
-			bags = bank_bags
-			core.bankrequired = true
+	function core.Compress(arg)
+		if arg=="help" then
+			return core.PrintHelp()
 		end
-	else
-		bags = player_bags
+		local bags
+		if arg=="bank" then
+			if not core.bank_open then
+				DEFAULT_CHAT_FRAME:AddMessage("BankStack: You must be at the bank to compress your bank bags.", 1, 0, 0)
+				return
+			else
+				bags = bank_bags
+				core.bankrequired = true
+			end
+		else
+			bags = player_bags
+		end
+		core.Stack(bags, bags, is_partial)
 	end
-	core.Stack(bags, bags)
 end
 function core.PrintHelp()
 	DEFAULT_CHAT_FRAME:AddMessage("BankStack: Stacks things.", 1, 1, 1)
@@ -95,13 +111,21 @@ function core.PrintHelp()
 	DEFAULT_CHAT_FRAME:AddMessage("/compress bank -- merges stacks in your bank", 1, 1, 1)
 end
 
-function core.Stack(source_bags, target_bags)
-	-- Fill incomplete stacks in target_bags with items from source_bags
-	-- This function just creates a list of slots to move to other slots, and passes it off to a worker function to do the moving.
+local function default_can_move() return true end
+function core.Stack(source_bags, target_bags, can_move)
+	-- Fill incomplete stacks in target_bags with items from source_bags.
+	-- source_bags: table, e.g. {1,2,3,4}
+	-- target_bags: table, e.g. {1,2,3,4}
+	-- can_move: function or nil.  Called as can_move(itemid, bag, slot)
+	--   for any slot in source that is not empty and contains an item that
+	--   could be moved to target.  If it returns false then ignore the slot.
+	-- Note: This function just creates a list of moves to make, then unhides
+	-- frame to get its OnUpdate going.
 	if current_link or current_target then
 		DEFAULT_CHAT_FRAME:AddMessage("BankStack: A stacker is already running.", 1, 0, 0)
 		return
 	end
+	if not can_move then can_move = default_can_move end
 	-- Model the target bags.
 	for _,bag in pairs(target_bags) do
 		local slots = GetContainerNumSlots(bag)
@@ -113,7 +137,7 @@ function core.Stack(source_bags, target_bags)
 				-- If this stack is full we don't care about it
 				-- Sadly, making this more general means we can't filter on GetItemCount(link) here, too.
 				if count ~= stack_size then
-					local id = bag_int(bag, slot)
+					local id = encode_bagslot(bag, slot)
 					target_items[link] = (target_items[link] and target_items[link] or 0) + 1
 					target_links[id] = link
 					target_sinks[id] = stack_size - count
@@ -126,15 +150,15 @@ function core.Stack(source_bags, target_bags)
 		local slots = GetContainerNumSlots(bag)
 		for slot=1, slots do
 			local link = link_to_id(GetContainerItemLink(bag, slot))
-			if link and target_items[link] then
+			if link and target_items[link] and can_move(link, bag, slot) then
 				--there's an item in this slot *and* we have room for more of it in the bank somewhere
-				local source_slot = bag_int(bag, slot)
+				local source_slot = encode_bagslot(bag, slot)
 				local count = select(2, GetContainerItemInfo(bag, slot))
 				for target_slot, target_link in pairs(target_links) do
 					if not target_items[link] then break end
 					if target_link == link and target_slot ~= source_slot then -- (can't stack to itself)
 						-- Schedule moving from this slot to the bank slot.
-						table.insert(moves, source_slot .. ":" .. target_slot)
+						table.insert(moves, encode_move(source_slot, target_slot))
 						-- Deal with the bank slot:
 						local room = target_sinks[target_slot]
 						if room > count then
@@ -195,18 +219,18 @@ function core.DoMoves()
 			return core.StopStacking("BankStack: Confusion. Stopping.")
 		end
 		-- Drop the item into the target slot
-		return PickupContainerItem(int_bag(current_target))
+		return PickupContainerItem(decode_bagslot(current_target))
 	end
 	current_link = nil
 	current_target = nil
 	
 	if #moves > 0 then
-		local source, target = string.match(table.remove(moves), "^(.+):(.+)$")
-		local source_bag, source_slot = int_bag(source)
-		--local target_bag, target_slot = int_bag(source)
+		local source, target = decode_move(table.remove(moves))
+		local source_bag, source_slot = decode_bagslot(source)
+		--local target_bag, target_slot = decode_bagslot(source)
 		local link = link_to_id(GetContainerItemLink(source_bag, source_slot))
 		local source_count = select(2, GetContainerItemInfo(source_bag, source_slot))
-		local target_count = select(2, GetContainerItemInfo(int_bag(target)))
+		local target_count = select(2, GetContainerItemInfo(decode_bagslot(target)))
 		local stack_size = select(8, GetItemInfo(link))
 		
 		current_link = link
