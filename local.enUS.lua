68e8bc8a4d4b84eddf187bcf0f4d2ac7d02147f6
David Lynch <kemayo@gmail.com>
2007-11-02 00:04:25 +0000

    Actually *add* the locale file.

---
+++ local.enUS.lua
@@ -0,0 +1,29 @@
+if not GetLocale()=='enUS' then return end
+BankStackLocale = {
+	-- Messages:
+	already_running = "BankStack: A stacker is already running.",
+	at_bank = "BankStack: You must be at the bank.",
+	complete = "BankStack: Complete.",
+	confused = "BankStack: Confusion. Stopping.",
+	moving = "BankStack: Moving %s.",
+	opt_set = "BankStack: %s set to %s.",
+	options = "BankStack options:",
+	perfect = "BankStack: Perfection already exists.",
+	to_move = "BankStack: %d moves to make.",
+	
+	-- Item types and subtypes:
+	ARMOR = "Armor",
+	BAG = "Bag",
+	CONSUMABLE = "Consumable",
+	CONTAINER = "Container",
+	GEM = "Gem",
+	KEY = "Key",
+	MISC = "Miscellaneous",
+	PROJECTILE = "Projectile"
+	REAGENT = "Reagent",
+	RECIPE = "Recipe",
+	QUEST = "Quest",
+	QUIVER = "Quiver",
+	TRADEGOODS = "Trade Goods",
+	WEAPON = "Weapon",
+}
