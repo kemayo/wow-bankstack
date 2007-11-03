ffcca24ada85b7c8fe2778fc95888d725d61eef1
David Lynch <kemayo@gmail.com>
2007-11-03 04:35:56 +0000

    Improve sort of trade goods.

--- sort.lua
+++ sort.lua
@@ -73,10 +73,63 @@ local inventory_slots = {
 	INVTYPE_RELIC = 24,
 	INVTYPE_TABARD = 25,
 }
+-- Trade goods have crappy subtypes.  They are "trade goods", "devices", "parts", and "explosives".  Yes, three for Engineering, one for the rest.
+local trade_goods = {
+	--primals and motes
+	primal = {23571, 22451, 22452, 21884, 21886, 22457, 22456, 21885, 22572, 22573, 22574, 22575, 22576, 22577, 22578,},
+	--tailoring
+	bolts = {2996, 2997, 4305, 4339, 14048, 21840, 21842, 21844,},
+	cloth = {2589, 2592, 4306, 4338, 14047, 21877, 23854, 23855,},
+	--mining
+	--gems = {}, --most gems are correctly under "gems", so.
+	stone = {2836, 12365, 2838, 7912, 2835},
+	ore = {2771, 10620, 3858, 2772, 23424, 11370, 23427, 2776, 23426, 2775, 7911, 23425, 2770, 11370,},
+	bars = {12360, 17771, 23447, 23448, 3577, 23449, 2842, 6037, 23446, 2841, 2840, 11371, 23445, 23573, 3575, 3860, 3859, 12359, 3576,},
+	--other
+	herbs = {13468, 19726, 22790, 8836, 8839, 22710, 2450, 2453, 13463, 22786, 2449, 3818, 22794, 22785, 4625, 22788, 8845, 13464, 3821, 3369, 8846, 13467, 3358, 3356, 3357, 785, 22793, 13465, 22791, 22797, 22792, 2447, 13466, 8831, 22787, 765, 3820, 8838, 2452, 22789, 3355, 8153, 3819,},
+	leather = {2934, 4236, 4231, 4233, 15407, 8172, 23793, 4234, 21887, 2318, 2319, 8170, 4304,},
+	cooking = {27422,27669,3173,3730,33823,3172,2677,27671,3404,21024,27677,769,5503,27678,2673,2886,2675,2674,33824,1081,22644,13888,8150,27435,27439,4655,12207,9061,27438,2251,723,731,12204,2692,27437,24477,5467,1015,13757,3731,27668,785,12223,2678,23676,730,1468,12037,3685,12184,31670,27674,6291,6308,13754,21153,6317,6289,8365,13759,6361,13758,6362,21071,6303,4603,13756,13760,13889,12203,20424,5466,31671,30817,4470,6889,4402,5465,5468,3713,3174,27425,5471,2665,27676,5469,729,2672,2452,27682,5504,12206,3667,12208,5470,12202,1080,3712,27681,12205,27429,7974,},
+	jewelcrafting = {31079, 20817, 20816, 20963, 21752,},
+}
+local trade_goods_order = {
+	primal = 0,
+	bolts = 1,
+	cloth = 2,
+	bars = 3,
+	ore = 4,
+	stone = 5,
+	herbs = 6,
+	leather = 7,
+	cooking = 8,
+	jewelcrafting = 9,
+	misc = 10,
+}
+local function get_trade_goods_order(id)
+	local cat
+	for k,t in pairs(trade_goods) do
+		for _,iid in pairs(t) do
+			if id==iid then
+				cat = k
+				break
+			end
+		end
+	end
+	if not cat then return -1 end
+	return trade_goods_order[cat]
+end
 
 local bag_sorted = {}
 local bag_ids = {}
 local bag_stacks = {}
+local function prime_sort(a, b)
+	local a_name, _, a_rarity, a_level, a_minLevel, a_type, a_subType, a_stackCount, a_equipLoc, a_texture = GetItemInfo(bag_ids[a])
+	local b_name, _, b_rarity, b_level, b_minLevel, b_type, b_subType, b_stackCount, b_equipLoc, b_texture = GetItemInfo(bag_ids[b])
+	if a_level == b_level then
+		return a_name < b_name
+	else
+		return a_level > b_level
+	end
+end
 local function default_sorter(a, b)
 	-- a and b are from encode_bagslot
 	-- note that "return a < b" would maintain the bag's state
@@ -123,22 +176,22 @@ local function default_sorter(a, b)
 				local b_equipLoc = inventory_slots[b_equipLoc] or -1
 				if a_equipLoc == b_equipLoc then
 					-- sort by level, then name
-					if a_level == b_level then
-						return a_name < b_name
-					else
-						return a_level > b_level
-					end
+					return prime_sort(a, b)
 				else
 					return a_equipLoc < b_equipLoc
 				end
 			else
 				if a_subType == b_subType then
-					-- sort by level, then name
-					if a_level == b_level then
-						return a_name < b_name
-					else
-						return a_level > b_level
+					if a_subType == L.TRADEGOODS then
+						local a_tgo = get_trade_goods_order(a_id)
+						local b_tgo = get_trade_goods_order(b_id)
+						if a_tgo == b_tgo then
+							return prime_sort(a, b)
+						else
+							return a_tgo < b_tgo
+						end
 					end
+					return prime_sort(a, b)
 				else
 					return a_subType < b_subType
 				end
