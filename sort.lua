769812eae5a0448a733a2f605f5617bb4a119ba3
David Lynch <kemayo@gmail.com>
2007-11-17 06:50:27 +0000

    Remove debug print!

--- sort.lua
+++ sort.lua
@@ -163,7 +163,6 @@ local function default_sorter(a, b)
 				if a_subType == b_subType then
 					return prime_sort(a, b)
 				else
-					AceLibrary("AceConsole-2.0"):Print(a_name, a_type, a_subType, item_subtypes[a_type][a_subType], b_name, b_type, b_subType, item_subtypes[b_type][b_subType])
 					return (item_subtypes[a_type][a_subType] or 99) < (item_subtypes[b_type][b_subType] or 99)
 				end
 			end
