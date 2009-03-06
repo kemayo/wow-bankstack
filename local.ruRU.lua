8d4a19107af6e450c2cc49cecc8a5b9ae5ce23d3
pr0ff <pr0ff@dummy.example.com>
2009-03-06 16:37:59 +0000

    Add ruRU locale

---
+++ local.ruRU.lua
@@ -0,0 +1,35 @@
+﻿--http://forums.playhard.ru/index.php?s=&showtopic=27938&view=findpost&p=535884
+if not GetLocale()=='ruRU' then return end
+BankStackLocale = {
+	-- Messages:
+	already_running = "BankStack: A stacker is already running.",
+	at_bank = "BankStack: вы должны быть в банке.",
+	complete = "BankStack: завершено.",
+	confused = "BankStack: Confusion. Stopping.",
+	moving = "BankStack: перемещение %s.",
+	opt_set = "BankStack: %s установлено в %s.",
+	options = "BankStack опции:",
+	perfect = "BankStack: Perfection already exists.",
+	to_move = "BankStack: %d moves to make.",
+	
+	-- Item types and subtypes:
+	ARMOR = "Доспехи",
+	BAG = "Сумка",
+	CONSUMABLE = "Потребляемые",
+	CONTAINER = "Сумки",
+	GEM = "Самоцветы",
+	KEY = "Ключ",
+	MISC = "Разное",
+	PROJECTILE = "Боеприпасы",
+	REAGENT = "Реагент",
+	RECIPE = "Рецепты",
+	QUEST = "Задания",
+	QUIVER = "Амуниция",
+	TRADEGOODS = "Ремесла",
+	WEAPON = "Оружие",
+}
+--Bindings:
+BINDING_HEADER_BANKSTACK = "BankStack"
+BINDING_NAME_BANKSTACK = "Stack to bank"
+BINDING_NAME_COMPRESS = "Сжать сумки"
+BINDING_NAME_BAGSORT = "Отсортировать сумки"
